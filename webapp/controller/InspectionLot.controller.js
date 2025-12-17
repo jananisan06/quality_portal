0f4005bef52349b9ea8dd2884f25dd863e0642f8
Janani K S <jananisan06@gmail.com>
2025-12-17 10:44:29 +0530

    feat: Implement Inspection Lot list view with new OData service integration and routing.

--- webapp/controller/InspectionLot.controller.js
+++ webapp/controller/InspectionLot.controller.js
@@ -1,41 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/odata/v2/ODataModel"
-], (Controller, UIComponent, ODataModel) => {
+    "sap/ui/core/UIComponent"
+], (Controller, UIComponent) => {
     "use strict";
 
     return Controller.extend("com.kaar.qualityportal.controller.InspectionLot", {
-        onInit() {
-            // Debugging: Explicitly create the model to verify connectivity
-            var sServiceUrl = "/sap/opu/odata/sap/ZKSJ_INSPECTION_QP_CDS/";
-            var oModel = new sap.ui.model.odata.v2.ODataModel(sServiceUrl, {
-                useBatch: false,
-                defaultBindingMode: "TwoWay"
-            });
-
-            oModel.attachMetadataLoaded(function () {
-                console.log("Explicit ODataModel: Metadata loaded successfully");
-            });
-
-            oModel.attachMetadataFailed(function (oEvent) {
-                console.error("Explicit ODataModel: Metadata failed to load", oEvent.getParameter("response"));
-            });
-
-            this.getView().setModel(oModel, "inspection");
-            console.log("Explicitly set 'inspection' model in onInit");
-        },
-
-        onRefresh: function () {
-            var oTable = this.byId("inspectionTable");
-            var oBinding = oTable.getBinding("items");
-            if (oBinding) {
-                oBinding.refresh();
-                sap.m.MessageToast.show("Refreshed");
-            } else {
-                sap.m.MessageToast.show("No binding found");
-            }
-        },
         onNavBack() {
             this.getOwnerComponent().getRouter().navTo("Dashboard");
         },
